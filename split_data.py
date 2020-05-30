ebd02af732378e87bef20e0145a89fc72c9e9739
XDong18 <1141191813@qq.com>
2020-05-30 15:01:59 -0700

    update

---
+++ split_data.py
@@ -0,0 +1,25 @@
+import os
+import random
+
+random.seed(0)
+path = './data_processed_vkt2'
+file_list = os.listdir(path)
+total_num = len(file_list)
+
+val_num = int(total_num / 11)
+train_num = total_num - val_num
+
+train_list = random.sample(file_list, train_num)
+val_list = random.sample(file_list, val_num)
+for fn in train_list:
+    rela_fn = os.path.join(path, fn)
+    new_fn = 'TRAIN_' + fn
+    new_rela_fn = os.path.join(path, new_fn)
+    os.rename(rela_fn, new_rela_fn)
+
+for fn in val_list:
+    rela_fn = os.path.join(path, fn)
+    new_fn = 'VAL_' + fn
+    new_rela_fn = os.path.join(path, new_fn)
+    os.rename(rela_fn, new_rela_fn)
+
