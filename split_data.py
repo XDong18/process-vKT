ff9b7a20301adf818d859850d3a6da615d059b8c
XDong18 <1141191813@qq.com>
2020-05-30 16:38:33 -0700

    update

--- split_data.py
+++ split_data.py
@@ -10,20 +10,17 @@ total_num = len(file_list)
 val_num = int(total_num / 11)
 train_num = total_num - val_num
 
-# train_list = random.sample(file_list, train_num)
-# val_list = random.sample(file_list, val_num)
-val_list = glob.glob(os.path.join(path, 'clone' + '*' + '.npz'))
-# for fn in train_list:
-#     rela_fn = os.path.join(path, fn)
-#     new_fn = 'TRAIN_' + fn
-#     new_rela_fn = os.path.join(path, new_fn)
-#     os.rename(rela_fn, new_rela_fn)
+train_list = random.sample(file_list, train_num)
+val_list = [i for i in file_list if i not in train_list]
+for fn in train_list:
+    rela_fn = os.path.join(path, fn)
+    new_fn = 'TRAIN_' + fn
+    new_rela_fn = os.path.join(path, new_fn)
+    os.rename(rela_fn, new_rela_fn)
 
 for fn in val_list:
-    # rela_fn = os.path.join(path, fn)
-    s_fn = os.path.split(fn)[-1]
-    # print(s_fn)
-    new_fn = 'VAL_' + s_fn
+    rela_fn = os.path.join(path, fn)
+    new_fn = 'VAL_' + fn
     new_rela_fn = os.path.join(path, new_fn)
-    os.rename(fn, new_rela_fn)
+    os.rename(rela_fn, new_rela_fn)
 
